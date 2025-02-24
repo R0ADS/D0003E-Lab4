e247c8ff44b48dc3d3ff6add259cbce2190ac06f
Road <antale-3@student.ltu.se>
2025-02-24 15:54:11 +0100

    first commit

---
+++ D0003E-lab4/main.c
@@ -0,0 +1,18 @@
+/*
+ * D0003E-lab4.c
+ *
+ * Created: 2025-02-24 15:52:04
+ * Author : anton
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
